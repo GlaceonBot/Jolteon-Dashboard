246a3f273cc69dd921f6cd373ea2c3ae04f541ba
valkyrie_pilot <gxhutchinson@gmail.com>
2021-07-25 05:43:13 +0000

    add tagslisting better

--- flask.sh
+++ flask.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd /home/Glaceon/Jolteon/dash
 source ./venv/bin/activate
 python -m pip install -r requirements.txt
-python -m gunicorn -b 0.0.0.0:33000 app:app
+python -m gunicorn --certfile=/etc/letsencrypt/live/mcfix.org/fullchain.pem --keyfile=/etc/letsencrypt/live/mcfix.org/privkey.pem -b 0.0.0.0:33000 app:app
